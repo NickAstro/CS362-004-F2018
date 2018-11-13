1a4d4ef540675a0c5eba278fcca3745d59f61711
Louie Nicastro <louis.m.nicastro@gmail.com>
2018-11-13 01:47:21 +0000

    implement card2 - ready for flip testing. updated makefile

---
+++ projects/nicastrl/dominion/randomtestcard1.c
@@ -0,0 +1,26 @@
+/*
+randomtestcard1.c is a random tester for the GreatHall card in Dominion.c
+Louis Nicastro
+Due 11/14/18
+*/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "interface.h"
+#include <math.h>
+#include <time.h>
+#include <assert.h>
+
+#define NUM_TESTS 1500
+
+typedef struct gameState gameState;
+int getHandPosition(int p, gameState *g, enum CARD card);
+
+int main()
+{
+    return 0;
+}
